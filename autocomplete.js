1e30286e9cfe42b6644e4ec8fb2473f2d26d2f43
Yoel Duran <yoet92@gmail.com>
2021-10-24 11:04:41 -0300

    autocomplete enhansment

--- autocomplete.js
+++ autocomplete.js
@@ -20,12 +20,11 @@ function autocomplete(inp, arr) {
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
             /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            if (arr[i].name.split('').some(e => e.localeCompare(val, 'en', {sensitivity: 'base'}))) {
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
-                b.innerHTML = "<strong>" + arr[i].name.substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].name.substr(val.length);
+                b.innerHTML += arr[i].name;
                 /*insert a input field that will hold the current array item's value:*/
                 b.innerHTML += "<input type='hidden' data-value='"+ arr[i].value +"' value='" + arr[i].name + "'>";
                 /*execute a function when someone clicks on the item value (DIV element):*/
